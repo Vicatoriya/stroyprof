baa685891e8908585b5550ec5f3d304c07b57ecd
STEPASHKA <i.eshcho.live@gmail.com>
2024-03-18 14:44:03 +0300

    +отделка парилок

--- src/Pages/OtdelkaParilok.js
+++ src/Pages/OtdelkaParilok.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Upsite from "../Components/Upsite/Upsite";
+import Header from "../Components/Header/Header";
+import First_section from "../Components/First_section/First_section";
+import Footer from "../Components/Footer/Footer";
+import Call from "../Components/Call/Call";
+import "./Remont.css";
+import data1 from "./data/forParilkas/DataForG1parilka";
+import G1 from "../Components/Additional_Pager/Galleries/Gallery1/G1";
+import Title from "../Components/Title/Title";
 export default function OtdelkaParilok(){
-    return(
-        <>
+    const [frstData, setData] = useState([]);
 
-        </>
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('frstsectParilki.json');
+                if (!response.ok) {
+                    throw new Error('Ошибка при загрузке файла');
+                }
+                const jsonData = await response.json();
+                setData(jsonData[0]);
+            } catch (error) {
+                console.error('ошибка при загрузке и обработке данных', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    return(
+        <div className={"remont"}>
+            <Upsite/>
+            <Header/>
+            <First_section data={frstData}/>
+            <Title title={"Примеры работ"} />
+            <G1 data={data1}/>
+            <Call/>
+            <Footer/>
+        </div>
     )
 }
---
+++ src/Pages/data/forParilkas/DataForG1parilka.js
@@ -0,0 +1,24 @@
+const data1 = [
+    {
+        image: "parilkiPhoto/1.jpg",
+        caption: ""
+    },
+    {
+        image: "parilkiPhoto/2.jpg",
+        caption: ""
+    },
+    {
+        image: "parilkiPhoto/3.jpg",
+        caption: ""
+    },
+    {
+        image: "parilkiPhoto/4.jpg",
+        caption: ""
+    },
+    {
+        image: "parilkiPhoto/5.jpg",
+        caption: ""
+    }
+];
+
+export default data1;
