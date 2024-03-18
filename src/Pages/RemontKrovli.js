29efbab7a1f8196bf76567ed13156a57ef49b00a
STEPASHKA <i.eshcho.live@gmail.com>
2024-03-18 14:09:24 +0300

    +ремонт

--- src/Pages/RemontKrovli.js
+++ src/Pages/RemontKrovli.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Upsite from "../Components/Upsite/Upsite";
+import First_section from "../Components/First_section/First_section";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import Call from "../Components/Call/Call";
+import "./Remont.css";
 export default function RemontKrovli(){
-    return(
-        <>
+    const [frstData, setData] = useState([]);
 
-        </>
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('frstsectRemont.json');
+                if (!response.ok) {
+                    throw new Error('Ошибка при загрузке файла');
+                }
+                const jsonData = await response.json();
+                setData(jsonData[0]);
+            } catch (error) {
+                console.error('ошибка при загрузке и обработке данных', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    return(
+        <div className={"remont"}>
+            <Upsite/>
+            <Header/>
+            <First_section data={frstData}/>
+            <h1></h1>
+            <Call/>
+            <Footer/>
+        </div>
     )
 }
