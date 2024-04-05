3d61f0b6e8e9d429e3b7d06f9410ceaed61821a5
STEPASHKA <i.eshcho.live@gmail.com>
2024-04-05 14:43:24 +0300

    fix

--- src/Components/First_section/First_section.js
+++ src/Components/First_section/First_section.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MyImage from '../../pics/a.jpg';
 import "./First_section.css";
 import '../../index.css'
 
 export default function First_section({data}){
     return(<div className='first_section'>
-    <img className='photo' src={MyImage}></img>
+    <img className='photo' src={data.image}></img>
         <div>
             <h1 className='text'>{data.title}</h1>
             <p className='text'>{data.frst}</p>
